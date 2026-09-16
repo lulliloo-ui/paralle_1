be1546d7317a96cb537d1aaa9fae89b109873f82
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-09-16 18:40:31 +0300

    clicker

---
+++ clicker.cpp
@@ -0,0 +1,14 @@
+#include "clicker.hpp"
+
+Clicker::Clicker():
+  start_(std::chrono::steady_clock::now())
+{}
+
+double Clicker::millisec() const
+{
+  using std::chrono::steady_clock;
+  using std::chrono::duration_cast;
+  using std::chrono::milliseconds;
+  auto t = steady_clock::now();
+  return duration_cast< milliseconds >(t - start_).count();
+}
